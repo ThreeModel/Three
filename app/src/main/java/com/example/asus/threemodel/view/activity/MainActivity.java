34c08faaf16ce3671a0008e0f570cb9a4b1020c9
qikaikaikaikai <qikaikai980324>
2018-05-17 14:06:41 +0800

    aaaaaq

--- app/src/main/java/com/example/asus/threemodel/view/activity/MainActivity.java
+++ app/src/main/java/com/example/asus/threemodel/view/activity/MainActivity.java
@@ -1,19 +1,41 @@
 package com.example.asus.threemodel.view.activity;
 
+import android.graphics.Color;
 import android.util.Log;
 import android.view.View;
 
 import com.example.asus.threemodel.R;
 import com.example.asus.threemodel.model.bean.MainBean;
 import com.example.asus.threemodel.presenter.presenter.MainPresenter;
+import com.example.asus.threemodel.view.fragment.ChoicenessFragment;
+import com.example.asus.threemodel.view.fragment.DiscoverFrament;
+import com.example.asus.threemodel.view.fragment.MyFrament;
+import com.example.asus.threemodel.view.fragment.SpecialFragment;
 import com.example.asus.threemodel.view.inter.IMainView;
+import com.hjm.bottomtabbar.BottomTabBar;
 
 public class MainActivity extends BaseActivity<MainPresenter> implements IMainView<MainBean> {
 
 
     @Override
     void initView() {
+        BottomTabBar btb = findViewById(R.id.btb);
+        btb.init(getSupportFragmentManager())
+                .setImgSize(60, 60)
+                .setFontSize(12)
+                .setTabPadding(4, 6, 10)
+                .setChangeColor(Color.RED, Color.GRAY)
+                .addTabItem("首页",R.drawable.found_select,R.drawable.found,ChoicenessFragment.class)
+                .addTabItem("西瓜视频",R.drawable.special_select,R.drawable.special,SpecialFragment.class)
+                .addTabItem("微头条",R.drawable.fancy_select,R.drawable.fancy,DiscoverFrament.class)
+                .addTabItem("登录",R.drawable.my_select,R.drawable.my,MyFrament.class)
+                .isShowDivider(true)
+                .setOnTabChangeListener(new BottomTabBar.OnTabChangeListener() {
+                    @Override
+                    public void onTabChange(int position, String name) {
 
+                    }
+                });
     }
 
     @Override
---
+++ app/src/main/java/com/example/asus/threemodel/view/fragment/ChoicenessFragment.java
@@ -0,0 +1,21 @@
+package com.example.asus.threemodel.view.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.asus.threemodel.R;
+
+public class ChoicenessFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = View.inflate(getContext(), R.layout.chioceness_frament, null);
+        return view;
+
+    }
+}
